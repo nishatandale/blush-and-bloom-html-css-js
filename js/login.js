ae9a028f083a90a2506f86252855253282db6c04
SayaliPawar1312 <sayalipawar1312@gmail.com>
2026-03-08 17:05:16 +0530

    changes

---
+++ js/login.js
@@ -0,0 +1,16 @@
+document.getElementById("loginForm").addEventListener("submit", function(e){
+
+e.preventDefault();
+
+let email = document.getElementById("email").value;
+let password = document.getElementById("password").value;
+
+if(email === "" || password === ""){
+alert("Please fill all fields");
+}
+else{
+alert("Login Successful");
+window.location.href="../index.html";
+}
+
+});
